25233cd1d3e49046934bbaa5c0d2181e20289cf8
DILLI KHATIWADA <dilli.74742@memc.tu.edu.np>
2023-03-10 10:54:38 +0545

    change in layout  and order partially

---
+++ LotusRMS.Models/Service/Implementation/OrderService.cs
@@ -0,0 +1,67 @@
+﻿using LotusRMS.Models.Dto.OrderDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotusRMS.Models.Service.Implementation
+{
+    public class OrderService : IOrderService
+    {
+        public Task<bool> AddOrderItem(AddOrderItemDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid Create(CreateOrderDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> CreateAsync(CreateOrderDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<LotusRMS_Order> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<LotusRMS_Order>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LotusRMS_Order GetByGuid(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LotusRMS_Order> GetByGuidAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid Update(UpdateOrderDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> UpdateAsync(UpdateOrderDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid UpdateStatus(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> UpdateStatusAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LotusRMS.Models/Viewmodels/Order/AddNewOrderVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotusRMS.Models.Viewmodels.Order
+{
+    public class AddNewOrderVM
+    {
+        public Guid MenuId { get; set; }
+        public string Item_Name { get; set; }
+
+        public float  Quantity { get; set; }
+        public float Rate { get; set; }
+        private float total { get; set; }
+        public float Total
+        {
+            get { return total; }
+            set { total = Quantity * Rate; }
+        }
+
+
+    }
+}
