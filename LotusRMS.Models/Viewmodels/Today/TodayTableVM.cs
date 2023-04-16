0bb085e2b6d82077f6dcdc35ab29476a63ea9cbf
Dilliram <rajukhatiwada.rk@gmail.com>
2023-04-16 15:59:53 +0545

    today module added

---
+++ LotusRMS.Models/Viewmodels/Today/TodayTableVM.cs
@@ -0,0 +1,16 @@
+﻿using LotusRMS.Models.Viewmodels.Table;
+using LotusRMS.Models.Viewmodels.Type;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotusRMS.Models.Viewmodels.Today
+{
+    public class TodayTableVM
+    {
+        public TypeVM Type { get; set; }
+        public List<TodayDetailVM> Table { get; set; }
+    }
+}
---
+++ LotusRms.DataAccess/Migrations/20230416063626_OrderTotalupdated.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LotusRMS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderTotalupdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GetTotal",
+                table: "LotusRMS_Order_Details");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "GetTotal",
+                table: "LotusRMS_Order_Details",
+                type: "float",
+                nullable: false,
+                defaultValue: 0f);
+        }
+    }
+}
