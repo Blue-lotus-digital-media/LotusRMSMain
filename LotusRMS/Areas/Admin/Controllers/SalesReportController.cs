aaaf46de465a1b720f3c8b28e94451c15b547e9c
Prajwal Giri <mr100giri@gmail.com>
2023-05-07 16:25:53 +0545

    pushing

---
+++ LotusRMS/Areas/Admin/Controllers/SalesReportController.cs
@@ -0,0 +1,17 @@
+﻿using LotusRMS.DataAccess.Constants;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LotusRMSweb.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    [Authorize(Roles = "SuperAdmin,Admin")]
+    public class SalesReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
