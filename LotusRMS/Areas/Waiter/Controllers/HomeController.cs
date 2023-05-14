823c2385fbd0e8db8d67e5bb5bcf4baa964fdde3
Prajwal Giri <mr100giri@gmail.com>
2023-05-14 11:39:21 +0545

    waiter dashboard

---
+++ LotusRMS/Areas/Waiter/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LotusRMSweb.Areas.Waiter.Controllers
+{
+    [Area("Waiter")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
